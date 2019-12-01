9b1e996a2bae6195bbc12dfa2ed41846b8ac93a1
Marlon Mueller-Soppart <mmuellersoppart@gmail.com>
2019-12-01 17:09:10 -0600

    smartly finds the anchors of each of the lines to the right kmeans applied (in one dimension)

--- CS415_MusicalNote.py
+++ CS415_MusicalNote.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pathlib
 import cv2
-import math
-import sys
-import random
 import time
-import bisect
-
+import statistics as stats
 from FolderHandler import FolderHandler
+import sklearn
+from sklearn.cluster import KMeans
 from CenteredImage import centeredImage
 
 
@@ -99,6 +97,44 @@ def main():
             print("ERROR: could not find any lines")
             break
 
+    while len(lines) > 12:
+        houghVotes = houghVotes + 20
+        print(f"lower threshold to {houghVotes}")
+        lines = cv2.HoughLines(imgCanny, rho=1, theta=np.pi / 180, threshold=houghVotes)
+        if houghVotes > 1000:
+            print("ERROR: We cannot get rid of the lines!")
+            break
+
+    # ********************
+    # Remove Unwanted Lines
+    print("3. Remove Unwanted Lines\n")
+    fewLinesStart = time.time()
+    # ********************
+
+    #filter out unwanted lines
+
+    rhos = []
+    thetas = []
+    for i in range(len(lines)):
+        for rho, theta in lines[i]:
+            rhos.append(rho)
+            thetas.append(theta)
+
+    #get the mode of the lines
+    thetasCopy = thetas.copy()
+    for index1 in range(len(thetasCopy)):
+        thetasCopy[index1] = round(thetasCopy[index1], 2)
+
+    thetaMode = stats.mode(thetasCopy)
+
+    #search for lines that deviate too much (20 degrees greater than the mode)
+    badLines = [i for i, x in enumerate(thetas) if abs(x - thetaMode) > .35]
+
+    #remove the lines that are too crooked
+    for index2 in sorted(badLines, reverse=True):
+        del rhos[index2]
+        del thetas[index2]
+
     for i in range(len(lines)):
         for rho, theta in lines[i]:
             print(rho, theta)
@@ -111,10 +147,53 @@ def main():
             y1 = int(y0 + 1000 * (a))
             x2 = int(x0 - 1000 * (-b))
             y2 = int(y0 - 1000 * (a))
-            cv2.line(imgCanny, (x1, y1), (x2, y2), 122, 2)
+            cv2.line(imgCanny, (x1, y1), (x2, y2), 170, 2)
+
+    print()
+
+    # ********************
+    # Find y-index of the 5 lines
+    print("4. y-index of lines\n")
+    fewLinesStart = time.time()
+    # ********************
+
+    rhos = np.array(rhos)
+    print(rhos.reshape(-1, 1))
+    #select the anchors of each line to the left
+    kmeans1 = KMeans(n_clusters=5)
+    kmeans1.fit(rhos.reshape(-1, 1))
+
+    # where the 5 ones are
+    print(kmeans1.cluster_centers_)
+
+    for cluster in kmeans1.cluster_centers_:
+        imgCanny = applyColorKernel(imgCanny, 1, int(cluster[0]), 9)
 
     # write initial lined image
-    wrkSpace1.writeImageToOutput(imgCanny, imgNameNoExtension + "_houghInit" + ".png")
+    wrkSpace1.writeImageToOutput(imgCanny, imgNameNoExtension + "_ClusterDots" + ".png")
 
+def applyColorKernel(workingImg, xPos, yPos, size):
+    """
+    :param maxKernel: object , kernel type max
+    :param workingImg: img - where the kernel will be applied to
+    :param kernel: max kernel - (a kernel with only 1s)
+    :param xPos: int - xPos on matrix coordinate (not in terms of theta or rho)
+    :param yPos: int - yPos on matrix coordinate (not in terms of theta or rho)
+    :return: bool True for max and F for not max
+    """
+
+    kernelMaxDistFromOrigin = int((size - 1) / 2)
+    allDistFromOrigin = list(range(-kernelMaxDistFromOrigin, kernelMaxDistFromOrigin + 1))
+
+    # go through all the values around the point (determined by kernel size)
+    for distY in allDistFromOrigin:
+        for distX in allDistFromOrigin:
+            try:
+                print(yPos + distY, xPos + distX)
+                workingImg[yPos + distY][xPos + distX] = 200
+            except IndexError:
+                pass
+
+    return workingImg
 
 main()
